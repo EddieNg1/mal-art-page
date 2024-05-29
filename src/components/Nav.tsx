14c36789d679fad3f8ed6ac2eeacbc7a50dfc586
Eddie Ng <eng42@myseneca.ca>
2024-05-28 20:01:08 -0400

    Update Nav - toggle hamburger

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,16 +15,41 @@ export default function Nav() {
             <path d="M2 2L428 2" stroke="#282828" strokeLinecap="round"/>
             </svg>
 
-            <img alt="Mal profile picture" />
+            <img alt="M" />
+
             <h1 className="text-lg font-bold">
                 <a href="/">DenrathM</a>
             </h1>
 
-            <div className="space-y-1">
+            {/*
+            <div className="flex gap-10">
+                <a href="/">Home</a>
+                <a href="/">Gallery</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+            </div>
+            */
+            }
+            <div
+                onClick={() => setToggled((prevToggle) => !prevToggle)}
+                className="space-y-1 cursor-pointer z-50 xl:hidden"
+            >
                 <span className="block h-0.5 w-7 bg-black"></span>
                 <span className="block h-0.5 w-7 bg-black"></span>
                 <span className="block h-0.5 w-7 bg-black"></span>
             </div>
+
+            {toggled && (
+                <div className="fixed left-0 top-0  z-40 flex h-screen
+                w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold">
+                    <div className="flex flex-col gap-10">
+                        <a href="/">Home</a>
+                        <a href="/">Gallery</a>
+                        <a href="/">About</a>
+                        <a href="/">Contact</a>
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
